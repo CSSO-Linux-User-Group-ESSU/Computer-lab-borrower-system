689a314bd697cadf2fa6ef2a44aabd2f25ee7b56
afable31 <afablewarren2004@gmail.com>
2024-10-07 00:49:54 +0800

    general update

--- borrower_app/models.py
+++ borrower_app/models.py
@@ -6,16 +6,15 @@ class BorrowerInfo(models.Model):
     last_name = models.CharField(max_length=20)
     first_name = models.CharField(max_length=20)
     middle_name = models.CharField(max_length=20)
-    projector_qty = models.IntegerField(5)
-    led_qty = models.IntegerField(5)
-    monitor_qty = models.IntegerField(5)
-    keyboard_qty = models.IntegerField(5)
-    mouse_qty = models.IntegerField(5)
-    cpu_qty = models.IntegerField(5)
-    ups_qty = models.IntegerField(5)
-    sub_cord_qty = models.IntegerField(5)
-    power_cord_qty = models.IntegerField(5)
-
+    projector_qty = models.IntegerField(max_length=5)
+    led_qty = models.IntegerField(max_length=5)
+    monitor_qty = models.IntegerField(max_length=5)
+    keyboard_qty = models.IntegerField(max_length=5)
+    mouse_qty = models.IntegerField(max_length=5)
+    cpu_qty = models.IntegerField(max_length=5)
+    ups_qty = models.IntegerField(max_length=5)
+    sub_cord_qty = models.IntegerField(max_length=5)
+    power_cord_qty = models.IntegerField(max_length=5)
 
     def __str__(self):
         return f" Borrower {self.last_name}, {self.first_name} {self.middle_name}."
